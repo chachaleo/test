1c4d7001cf02f5a741eb562c385db3bce259ecec
chachaleo <charlotte.leo@hotmail.com>
2023-08-14 12:36:46 +0200

    modif db val

--- get_first_answer.php
+++ get_first_answer.php
@@ -4,16 +4,18 @@ $db_user = 'chachaleo';
 $db_pass = 'M5ZJwKF2F5SqKns';
 $db_name = 'impact_assess';
 
+
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$query = "SELECT `Col 2` as answer FROM questions_1 LIMIT 1";
+$query = "SELECT `col 2` as answer FROM questions LIMIT 1";
 $result = mysqli_query($conn, $query);
 
 if ($row = mysqli_fetch_assoc($result)) {
+    header('Content-Type: application/json'); // Set JSON content type
     echo json_encode($row['answer']);
 } else {
     echo json_encode("No answer found.");
@@ -21,4 +23,3 @@ if ($row = mysqli_fetch_assoc($result)) {
 
 mysqli_close($conn);
 ?>
-
